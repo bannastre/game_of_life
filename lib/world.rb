485a5cc8fa155d07c5ef578a075ecc65b223fd33
Christopher Harrop <bannastre@gmail.com>
2017-07-16 16:59:22 +0100

    World can see and switch cell state

--- lib/world.rb
+++ lib/world.rb
@@ -4,8 +4,15 @@ class World
   attr_reader :cells
 
   def initialize(x_axis, y_axis)
-    @cells = Array.new(x_axis) { Array.new(y_axis) }
+    @cells = Array.new(x_axis) { Array.new(y_axis, Cell.new) }
   end
 
+  def switch_cell(x_axis, y_axis)
+    @cells[x_axis][y_axis].switch
+  end
+
+  def cell_state(x_axis, y_axis)
+    @cells[x_axis][y_axis].state
+  end
 
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,12 +1,20 @@
 require 'world'
 
 describe World do
-  subject(:world) { described_class.new }
+  subject(:world) { described_class.new(3, 3) }
 
   it 'knows a number of cells equal to the width multiplied by the height' do
-    world = World.new(3, 3)
     expect(world.cells.length).to eq(3)
     expect(world.cells[0].length).to eq(3)
+  end
+
+  it 'can easily specify the state of any cell' do
+    expect(world.cell_state(2,1)).to eq(:dead)
+  end
+
+  it 'can switch the state of any cell' do
+    world.switch_cell(2,1)
+    expect(world.cell_state(2,1)).to eq(:alive)
   end
 
 end
